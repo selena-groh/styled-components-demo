b62cf1591a4633e4571c2b80b4d711b17b27543b
Selena Groh <selena.groh@gmail.com>
2020-07-13 09:23:12 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Demo from "./demo";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Styled Components Demo</h1>
+      <Demo />
+    </div>
+  );
+}
